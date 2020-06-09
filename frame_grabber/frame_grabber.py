a5bbae82555af7122dc1ecb4b2b34f2a9e97bef0
Anthony Spears <aspears@gatech.edu>
2020-06-09 11:11:13 -0400

    changed output to kitti dataset format

--- frame_grabber/frame_grabber.py
+++ frame_grabber/frame_grabber.py
@@ -1,12 +1,14 @@
 # frame_grabber.py - Frame grabber for full resolution stills from video
 # Mainained by Anthony Spears aspears@gatech.edu
+# Internal note: Using virtualenvwrapper - $ workon frame_grabber 
+# The output png files are named based on KITTI dataset formats
 
 import ffmpeg  
 import cv2
 import sys
 
 #########################################################################
-in_filename = '/media/aspears3/Data/test.mov'
+in_filename = '/media/aspears3/Data/icefin_2018_barne_glacier_seafloor.mov'
 fps_out = 5
 #########################################################################
 
@@ -23,19 +25,20 @@ ok, frame = in_vid.read()
 if not ok:
     print('Can\'t read video file')
     sys.exit()
-cv2.imwrite("frame%d.png" % 0, frame)
+cv2.imwrite("%06d.png" % 0, frame)
 
-width, height = frame.shape[1], frame.shape[0]
-
+width, height = frame.shape[1], frame.shape[0
+]
 # Get frame rate of input video.
 fps = in_vid.get(cv2.CAP_PROP_FPS)
-print("fps:", fps)
+print("input video fps:", fps)
 
 frame_counter = 0
 
 while True:
     if ((frame_counter % (fps/fps_out)) == 0):
-        cv2.imwrite("frame%d.png" % frame_counter, frame)
+        cv2.imwrite("%06d.png" % frame_counter, frame)
+        print("%06d.png" % frame_counter)
 
     # Read a new frame
     ok, frame = in_vid.read()
